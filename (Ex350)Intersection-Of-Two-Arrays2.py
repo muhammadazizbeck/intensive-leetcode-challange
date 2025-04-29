2a29c6777afa95f305beb36057d1f79b6079efa9
A1909bek <aa2004bek@gmail.com>
2025-04-29 20:33:27 +0500

    next commit

---
+++ (Ex350)Intersection-Of-Two-Arrays2.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        # res = []
+        # for num1 in nums1:
+        #     if num1 in nums2:
+        #         res.append(num1)
+        #         nums2.remove(num1)
+        # return res
