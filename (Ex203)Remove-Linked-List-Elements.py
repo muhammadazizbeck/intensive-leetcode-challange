7891144b03e25a4b3d1376e6f835e3c0c69f838a
A1909bek <aa2004bek@gmail.com>
2025-05-01 20:11:48 +0500

    next commit

---
+++ (Ex203)Remove-Linked-List-Elements.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: Optional[ListNode]
+        :type val: int
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode()
+        dummy.next = head
+        # prev = dummy
+        current = dummy
+        while current.next:
+            if current.next.val == val:
+                current.next = current.next.next
+            else:
+                current = current.next
+        return dummy.next
