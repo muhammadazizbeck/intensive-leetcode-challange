672f3bab393918aeecd6bf41c274a12534604380
A1909bek <aa2004bek@gmail.com>
2025-05-08 22:46:33 +0500

    adding solution

---
+++ (Ex2540)Minimum-Common-Value.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def getCommon(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: int
+        """
+        i = j = 0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                return nums1[i]
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                j += 1
+        return -1
+
+        # s = set(nums1)  # O(n)
+        # for num in nums2:  # O(m)
+        #     if num in s:
+        #         return num
+        # return -1
