e7b35e5f21a6d8a224765664cd7db5765c6c35c7
A1909bek <aa2004bek@gmail.com>
2025-06-07 13:14:55 +0500

    adding some changes

---
+++ (Ex1929)Concatenation-of-Array.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        # solution1
+        # arr1 = []
+        # for i in range(len(nums)):
+        #     arr1.append(nums[i])
+        # return nums+arr1
+
+        # solution2
+        return nums+nums
