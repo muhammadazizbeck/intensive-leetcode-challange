85104a9607298d97966dc64426e2ee5e58caa4d1
A1909bek <aa2004bek@gmail.com>
2025-06-07 22:17:15 +0500

    adding some changes

---
+++ (Ex771)Jewels-and-Stones.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        lister = [stone for stone in stones if stone in jewels]
+        return len(lister)
